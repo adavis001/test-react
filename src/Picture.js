68ae66c38719378cc656573685212e5cfcaec4f7
Alexander Davis <akonidavis@gmail.com>
2017-07-26 09:48:03 -1000

    things are rotating

--- src/Picture.js
+++ src/Picture.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from './Button';
 import Random from './Random';
+import './Picture.css';
 
 const companionCube = {
   src: 'https://i1.theportalwiki.net/img/thumb/3/34/Portal_Companion_Cube.png/175px-Portal_Companion_Cube.png',
@@ -11,10 +12,11 @@ const companionCube = {
 export class Picture extends React.Component {
   render() {
     return (
-      <div>
+      <div className='Main'>
       <img
         src={companionCube.src}
-        onClick={this.props.onClick}>
+        onClick={this.props.onClick}
+        className='Cube'>
       </img>
       Does it work?
       </div>
