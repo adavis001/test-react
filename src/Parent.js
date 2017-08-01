c8e41d355374035a86eb76b0a963629c89a12062
Alexander Davis <akonidavis@gmail.com>
2017-08-01 10:16:35 -1000

    passing props and state practice

--- src/Parent.js
+++ src/Parent.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Child } from './Child';
 
 class Parent extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = { name: 'Frarthur' };
+
+    this.changeName = this.changeName.bind(this);
   }
 
+  changeName(newName) {
+    this.setState({
+      name: newName
+    });
+  }
+
+
+
   render() {
-    return <div></div>;
+    return <Child name={this.state.name} onChange={this.changeName} />
   }
 }
+
+ReactDOM.render(
+  <Parent />,
+  document.getElementById('app')
+);
