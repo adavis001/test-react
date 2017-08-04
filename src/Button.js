1bb27c88e59ec3525cb597c7af6164e881da2aa0
Alexander Davis <akonidavis@gmail.com>
2017-08-04 11:25:34 -1000

    added containers

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+const styles = {
+  background: 'lightblue',
+  color: 'darkred'
+};
+
 export class Button extends React.Component {
   render() {
     return (
       <button
+        style={styles}
         className={ this.props.light ? 'light-button' : 'dark-button' }
         onClick={this.props.onClick} >
         Refresh
